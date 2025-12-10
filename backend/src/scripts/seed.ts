d82d075997b473cc7e3a563dff9aa37cf265c03b
yukito-takeuchi <iyggf66974@yahoo.ne.jp>
2025-12-10 15:30:09 +0900

    seed　エラー対応

--- backend/src/scripts/seed.ts
+++ backend/src/scripts/seed.ts
@@ -6,6 +6,7 @@ dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
 
 async function seedMasterData(client: any) {
